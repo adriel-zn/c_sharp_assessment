a866ff552c806455120ef7bec37ceb9e640fd051
Adriel Mariappen <adriel@softidoc.co.za>
2022-10-25 23:39:41 +0200

    Added Api controller methods

---
+++ Account_Ledger.Api/Controllers/AccountsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Account_Ledger.Api.Controllers
+{
+    [ApiController]
+    [Route("api/accounts")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly ILogger<AccountsController> _logger;
+
+        public AccountsController(ILogger<AccountsController> logger)
+        {
+            _logger = logger;
+        }   
+
+        [HttpPost]
+        public ActionResult AddAccount(dynamic account)
+        {
+            return new OkObjectResult(null);
+        }
+    }
+}
