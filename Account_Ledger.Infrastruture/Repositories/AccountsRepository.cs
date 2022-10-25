12989b16c0fd45cb3d4883d4005057f042eea9fe
Adriel Mariappen <adriel@softidoc.co.za>
2022-10-26 00:24:54 +0200

    Commit the class library

---
+++ Account_Ledger.Infrastruture/Repositories/AccountsRepository.cs
@@ -0,0 +1,11 @@
+﻿using Account_Ledger.Infrastruture.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Account_Ledger.Infrastruture.Repository
+{
+    public class AccountsRepository: IAccountsService
+    {
+    }
+}
---
+++ Account_Ledger.Infrastruture/Services/IAccountsService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Account_Ledger.Infrastruture.Services
+{
+    public interface IAccountsService
+    {
+    }
+}
